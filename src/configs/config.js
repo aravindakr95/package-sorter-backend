bf1e6489caa59da69bc80e13debedde820a71b1f
Aravinda Rathnayake <aravindacloud@gmail.com>
2021-04-24 20:48:56 +0530

    Revert prot 5000 to 3000

--- src/configs/config.js
+++ src/configs/config.js
@@ -22,7 +22,7 @@ const config = {
   },
   deployment: {
     host: process.env.HOST || '127.0.0.1', // DEMONSTRATION PURPOSES ONLY
-    port: process.env.PORT || 5000,
+    port: process.env.PORT || 3000,
   },
 };
 
